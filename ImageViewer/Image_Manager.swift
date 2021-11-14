8fe4327b82434b682f706f48d492a51915960f0e
user199544 <user199544@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2021-11-14 18:08:03 -0500

    Assignment3

---
+++ ImageViewer/Image_Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Image_Manager.swift
+//  ImageViewer
+//
+//  Created by user199544 on 11/9/21.
+//
+
+import Foundation
---
+++ ImageViewer/Imageinfo.swift
@@ -0,0 +1,8 @@
+//
+//  Imageinfo.swift
+//  ImageViewer
+//
+//  Created by user199544 on 11/9/21.
+//
+
+import Foundation
